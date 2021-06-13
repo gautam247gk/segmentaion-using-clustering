3cbb02f467db6243e75ac2ae55fbde5677b70edc
Gautam Krishnan <gautam247gk@gmail.com>
2021-06-13 12:03:01 +0530

    second attenpt

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-app.listen(3000,'0.0.0.0');
+app.listen(process.env.PORT||3000);
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
