e92eb798ef44831b2d698c2d219a5aa94ca9d70c
Gautam Krishnan <gautam247gk@gmail.com>
2021-06-13 15:01:33 +0530

    hello

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ function ps(req,res)
 app.get('/web',(req,res)=>{
     const rtype=req.query.RequestType;
     const date=req.query.Date;
+    const username=req.query.Username;
+    const password=req.query.Password;
     if(rtype=='Quarter')
     {
         process=spawn('python3',['./mc/quarter.py']);
@@ -58,7 +60,7 @@ app.get('/web',(req,res)=>{
         process=spawn('python3',['./mc/trendsrequest.py']);
         process.stdout.on('data', function(data) {
             //console.log(data.toString());
-            res.send(data.toString());
+            res.send(data.toJson());
         })
         process.stderr.on('data', (data) => {
             console.log(`error:${data}`);
@@ -66,5 +68,23 @@ app.get('/web',(req,res)=>{
         process.on('close', () => {
             console.log("Closed");
         });
+    }
+    else if(rtype=='Auth')
+    {
+        if(username=='John240') AND (password=='240john')
+        {
+            details = {
+                "Name" : "John Lennon" ,
+                "Username" : "John240",
+                "Email" : "john240@gmail.com",
+                "Phone" : "269-748-9882",
+                "Gender" : "Male",
+                "DOB" : "10/04/1997"
+            }
+            res.send(details.toJson);
+
+        }
+        
+        
     }
 });
