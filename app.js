e669e9eec6ae6d41e14856b19f8e6811bf5ae21f
Gautam Krishnan <gautam247gk@gmail.com>
2021-06-13 12:14:11 +0530

    first error after deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-app.listen(process.env.PORT||3000);
+app.listen(process.env.PORT || 3000);
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
